45ee99b4f2da2085578f6e6ff7d5465a9ed28cc8
rgopisettiaws12 <140700511+rgopisettiaws12@users.noreply.github.com>
2024-03-28 13:31:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "example" {
+  name                = "example-publicip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = "eastus"
+  allocation_method   = "Dynamic"
+}
+
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
   location            = azurerm_resource_group.rg.location
@@ -40,6 +47,7 @@ resource "azurerm_network_interface" "example" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
