d8fe6aadd12b686c0ba6e37524fd13b2aa8fc732
rgopisettiaws12 <140700511+rgopisettiaws12@users.noreply.github.com>
2024-03-28 11:51:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "default" {
-  name = "container-registry-rg"
+  name     = "container-registry-rg"
   location = "East US 2"
 
   tags = {
