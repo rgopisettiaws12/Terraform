8436e8bdf10a8324c7f537f1ff8ec478fe95f148
rgopisettiaws12 <140700511+rgopisettiaws12@users.noreply.github.com>
2024-03-28 11:07:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,65 +2,33 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name = "myResourceGroup"
-  location = "East US"
-}
+resource "azurerm_resource_group" "default" {
+  name = "container-registry-rg"
+  location = "East US 2"
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = "myVnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "mySubnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "myNIC"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "myNICConfg"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
+  tags = {
+    environment = "production"
   }
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  name                  = "myVM"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_DS1_v2"
+resource "azurerm_container_registry" "acr" {
+  name                = "kspcontainerregistry"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = "East US 2"
+  sku                 = "Standard"
+  admin_enabled       = true
+}
 
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
 
-  storage_os_disk {
-    name              = "myosDisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
 
-  os_profile {
-    computer_name  = "myVM"
-    admin_username = "adminuser"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
