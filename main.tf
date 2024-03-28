9d225f0d7877c02beb79b3d4e670b5cd992edcb7
rgopisettiaws12 <140700511+rgopisettiaws12@users.noreply.github.com>
2024-03-28 12:35:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,19 +2,75 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "default" {
-  name     = "container-registry-rg"
-  location = "East US 2"
+resource "azurerm_resource_group" "rg" {
+  name     = "TerraformTesting"
+  location = "eastus"
 
   tags = {
     environment = "production"
   }
 }
 
+resource "azurerm_availability_set" "DemoAset" {
+  name                = "example-aset"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  availability_set_id = azurerm_availability_set.DemoAset.id
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
 resource "azurerm_container_registry" "acr" {
-  name                = "kspcontainerregistry123"
-  resource_group_name = azurerm_resource_group.default.name
-  location            = "East US 2"
+  name                = "kspcontainerregistry1234"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = "eastus"
   sku                 = "Standard"
   admin_enabled       = true
 }
